64123ef2b5bbb56a8ea567aaf4d1242239a1eaf4
zihad <tazihad@gmail.com>
2025-03-25 23:49:06 +0600

    chore: :recycle: removed fedora testing

--- kmods/nvidia/scripts/build/00-prebuild.sh
+++ kmods/nvidia/scripts/build/00-prebuild.sh
@@ -12,9 +12,3 @@ rpm-ostree install \
   --uninstall rpmfusion-free-release-$(rpm -E %fedora)-1.noarch \
   --uninstall rpmfusion-nonfree-release-$(rpm -E %fedora)-1.noarch
 
-# If REPOSITORY_TYPE is set to true, enable the RPMFusion testing repos
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
-if [[ "${REPOSITORY_TYPE}" == "testing" ]]; then
-  echo "INFO: Enabling RPMFusion testing repositories"
-  sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-{free,nonfree}-updates-testing.repo
-fi
