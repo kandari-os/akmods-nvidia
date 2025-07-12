bf8ae22b5374e813a101351410c3f7eaed4d7121
tazihad <tazihad@gmail.com>
2025-07-12 13:31:08 +0600

    switching to dnf5 during build

--- kmods/nvidia/scripts/install/01-install.sh
+++ kmods/nvidia/scripts/install/01-install.sh
@@ -3,13 +3,14 @@
 set -ouex pipefail
 source /tmp/akmods/info/nvidia-vars
 
-rpm-ostree install /tmp/akmods/rpms/nvidia-addons-*.rpm
+dnf5 -y install \
+    /tmp/akmods/rpms/nvidia-addons-*.rpm
 
 # Enable nvidia-container-toolkit repo
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/nvidia-container-toolkit.repo
 
 # Install Nvidia drivers
-rpm-ostree install \
+dnf5 -y install \
     xorg-x11-drv-nvidia-{,cuda-,devel-,kmodsrc-,power-}${NVIDIA_FULL_VERSION} \
     nvidia-container-toolkit nvidia-vaapi-driver \
     /tmp/akmods/rpms/kmod-nvidia-${KERNEL_VERSION}-${NVIDIA_AKMOD_VERSION}.fc${RELEASE}.rpm
