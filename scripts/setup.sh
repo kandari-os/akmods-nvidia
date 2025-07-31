fac7c64f4c02a5816c5f0befba67af72962a909f
tazihad <tazihad@gmail.com>
2025-08-01 01:01:21 +0600

    fix: replace dnf5 with dnf in setup script and clean up comments

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,24 +5,19 @@
 
 set -oeux pipefail
 
-### PREPARE REPOS
 ARCH="$(rpm -E '%_arch')"
 RELEASE="$(rpm -E '%fedora')"
 
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/fedora-updates-archive.repo
 
-# enable RPMs with alternatives to create them in this image build
 mkdir -p /var/lib/alternatives
 
-#curl -LsSf -o /etc/yum.repos.d/fedora-coreos-pool.repo \
-#    https://raw.githubusercontent.com/coreos/fedora-coreos-config/stable/fedora-coreos-pool.repo
-
-dnf5 -y install \
+dnf -y install \
     kernel-devel \
     kernel-devel-matched
 
-dnf5 -y install \
+dnf -y install \
     akmods \
     mock
 
@@ -35,8 +30,6 @@ fi
 install -Dm644 /tmp/certs/public_key.der   /etc/pki/akmods/certs/public_key.der
 install -Dm644 /tmp/certs/private_key.priv /etc/pki/akmods/private/private_key.priv
 
-# protect against incorrect permissions in tmp dirs which can break akmods builds
 chmod 1777 /tmp /var/tmp
 
-# create directories for later copying resulting artifacts
 mkdir -p /var/cache/rpms/{kmods,kandari}
