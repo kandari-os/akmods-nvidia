f81f2132c7c7027fa12424dd748921fe7690df87
tazihad <tazihad@gmail.com>
2025-08-01 00:28:19 +0600

    refactor: update Nvidia driver installation command for clarity

--- kmods/nvidia/scripts/install/01-install.sh
+++ kmods/nvidia/scripts/install/01-install.sh
@@ -11,6 +11,8 @@ sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/nvidia-container-toolkit.repo
 
 # Install Nvidia drivers
 dnf -y install \
-    xorg-x11-drv-nvidia-{,cuda-,devel-,kmodsrc-,power-}${NVIDIA_FULL_VERSION} \
-    nvidia-container-toolkit nvidia-vaapi-driver \
+    xorg-x11-drv-nvidia-${NVIDIA_FULL_VERSION} \
+    xorg-x11-drv-nvidia-cuda-${NVIDIA_FULL_VERSION} \
+    nvidia-container-toolkit \
+    nvidia-vaapi-driver \
     /tmp/akmods/rpms/kmod-nvidia-${KERNEL_VERSION}-${NVIDIA_AKMOD_VERSION}.fc${RELEASE}.rpm
